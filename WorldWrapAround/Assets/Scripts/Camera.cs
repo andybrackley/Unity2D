c77b0dafd03d0a616dfc266b00f913650f0c3468
Andrew Brackley <andybrackley@hotmail.com>
2022-06-22 18:45:52 +0100

    Setup demo project to show how to have an infinitely scrolling horizontal background.

---
+++ WorldWrapAround/Assets/Scripts/Camera.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] float speed = 1;
+    private float z;
+    private Vector3 position;
+
+    private float left = -1.5f;
+    private float right = 1.5f;
+    private float leftMax = -2.5f;
+    private float rightMax = 2.5f;
+
+    [SerializeField] Transform leftCam;
+    [SerializeField] Transform rightCam;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.z = transform.position.z;
+        this.position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var present = this.position;
+        var horz = Input.GetAxis("Horizontal");
+
+        position += Vector3.right * horz * this.speed * Time.deltaTime;
+    
+        if(position.x < this.leftMax)
+        {
+            this.position = new Vector3(this.right, 0, this.z);
+        }
+        else if(position.x > this.rightMax)
+        {
+            this.position = new Vector3(this.left, 0, this.z);
+        }
+    }
+
+    void LateUpdate() {
+        transform.position = this.position;
+        this.leftCam.position = new Vector3(-4f, 0, this.z) + transform.position;
+        this.rightCam.position = new Vector3(4f, 0, this.z) + transform.position;
+    }
+}
---
+++ WorldWrapAround/Assets/readme.md
@@ -0,0 +1,12 @@
+# WorldWrapAround
+
+Demonstrates how to have an infinite scrolling background similar to that used in the Defender Game.
+
+### Tutorials:
+
+https://www.youtube.com/watch?v=KV3AEiYpdEo
+
+### Code References:
+
+https://github.com/codehoose/world-wrap-shooter
+
